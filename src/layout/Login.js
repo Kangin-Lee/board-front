45276652e514aae1ebe84583269bc09e8ec36be4
kangin <kangin@i-ops.co.kr>
2024-01-03 17:36:31 +0900

    로그인 페이지 개발 중

--- src/layout/Login.js
+++ src/layout/Login.js
@@ -4,14 +4,12 @@ const Login = () => {
   return (
     <div className='login-area'>
         <div className='login-wrapper'>
-            <img className='login-logo' width={180} src='/images/logo/logo-main.png'/>
+            <img className='login-logo' width={130} src='/images/logo/logo-main.png'/>
+            <h2>Login</h2>
             <form method='post' action='' id='login-form'>
                 <input type='text' name='userName' placeholder='Email' className='login-input'/>
                 <input type='password' name="userPassword" placeholder='Password' className='login-input'/>
-                <label for="remember-check">
-                    <input type='checkbox' id='remember-check' value='아이디 저장하기'/>
-                </label>
-                <input type='submit' value="Login"/>
+                <input type='submit' value="Login" className='login-submit'/>
             </form>
         </div>
     </div>
