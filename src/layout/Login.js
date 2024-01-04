fcf052f16b4e13beeeb8d1b05c40bdcd4aa13acb
kangin <kangin@i-ops.co.kr>
2024-01-04 15:01:46 +0900

    로그인 화면 구현 중...

--- src/layout/Login.js
+++ src/layout/Login.js
@@ -1,16 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const onSubmit = (e) =>{
+    e.preventDefault();
+  }
+
+
   return (
     <div className='login-area'>
         <div className='login-wrapper'>
-            <img className='login-logo' width={130} src='/images/logo/logo-main.png'/>
-            <h2>Login</h2>
-            <form method='post' action='' id='login-form'>
+            <img className='login-logo' width={200} src='/images/logo/logo-main.png'/>
+            <div>
+              <h2>Login</h2>
+              <form method='post' action='' id='login-form' onSubmit={onSubmit}>
+                <h5>Email</h5>
                 <input type='text' name='userName' placeholder='Email' className='login-input'/>
+                <h5>Password</h5>
                 <input type='password' name="userPassword" placeholder='Password' className='login-input'/>
                 <input type='submit' value="Login" className='login-submit'/>
-            </form>
+              </form>
+              <p className='go-to-sign-up'>아직 회원이 아니신가요? <Link to="/sign-up">회원가입</Link></p>
+            </div>
+
         </div>
     </div>
   )
